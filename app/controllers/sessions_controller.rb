220bf1b707fc3cc5ef514aec8aa2b799f79ee7ec
coolbooks28 <coolbooks28@gmail.com>
2011-10-14 15:54:47 -0700

    Done with user edit/update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  
   def new
     @title = "Sign in"
   end
@@ -14,7 +15,7 @@ class SessionsController < ApplicationController
     else
       # Sign the user in and redirect to the user's show page
       sign_in user
-      redirect_to user
+      redirect_back_or user
     end
   end
   
